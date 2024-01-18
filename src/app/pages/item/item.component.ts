65f6c3ee4c41e52558c6359e55cc91483e4e675b
Isa Leon <isaleon.v18@gmail.com>
2024-01-18 20:21:36 +0100

    app-item component

---
+++ src/app/pages/item/item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css'
+})
+export class ItemComponent {
+
+}
