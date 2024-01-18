eaf03257400e992909e223ed593a23b261bb2a8e
Isa Leon <isaleon.v18@gmail.com>
2024-01-18 20:53:56 +0100

    routing definition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './pages/body/body.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+//Routing definition
+const routes: Routes = [
+  { path: '', component:BodyComponent},
+  { path: 'about', component:AboutComponent},
+  { path: 'item', component:ItemComponent},
+
+//Routing exception
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
