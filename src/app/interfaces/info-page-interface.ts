c28f47c4772a8aec4b0713544c75a9513eaff8fe
Isa Leon <isaleon.v18@gmail.com>
2024-01-21 23:40:13 +0100

    interface

---
+++ src/app/interfaces/info-page-interface.ts
@@ -0,0 +1,11 @@
+export interface InfoPage {
+    title?: string;
+    email?: string;
+    name?: string;
+    author?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    tumblr?: string;
+    workteam?: any[];
+}
--- src/app/services/page-info.service.ts
+++ src/app/services/page-info.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPage } from '../interfaces/info-page-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PageInfoService {
+  info: InfoPage = {};
+  loaded = false;
 
   constructor(private http: HttpClient) {
 
-    //Read Json
+    //Read Json file
     this.http.get('assets/data/data-page.json')
-      .subscribe(resp => {
+      .subscribe((resp: InfoPage) => {
+        this.loaded = true;
+        this.info = resp;
 
         console.log(resp);
       });
